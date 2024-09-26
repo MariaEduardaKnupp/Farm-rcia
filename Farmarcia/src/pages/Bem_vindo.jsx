8c0203bc423eb80c929c8c9bcbc32b92d12a64d4
MariaEduardaKnupp <mariaeduardaknuppratto@gmail.com>
2024-09-26 17:28:21 -0300

    Prejeto da farmarcia

---
+++ Farmarcia/src/pages/Bem_vindo.jsx
@@ -0,0 +1,20 @@
+
+function Bem_vindo() {
+  return (
+    <div>
+
+<header className='header'>
+<img src="farmarcia-logo-pq.PNG.png" alt="" /> 
+<h1 className='farm'>Farmárcia</h1>
+</header>
+
+ <div className='segundo'>
+<h1>Bem Vindo a Farmárcia</h1>
+<button className="butin">Entrar</button>
+</div> 
+
+    </div>
+  )
+}
+
+export default Bem_vindo
